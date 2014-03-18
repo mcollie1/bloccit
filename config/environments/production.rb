708be78c4cb40c1a674f02ca30d69edee7c3dab3
mcollie1 <marcus.collier7@yahoo.com>
2014-03-17 22:40:50 -0400

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,7 @@ Bloccit::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.eager_load = true
+  # config email for production
+  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
 end
 
