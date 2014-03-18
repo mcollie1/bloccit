c5ab3f8aaafbeb290c89f2390aa8b7ab6cf4e059
mcollie1 <marcus.collier7@yahoo.com>
2014-03-17 22:15:30 -0400

    rails bin update for Heroku

---
+++ config/unicorn.rb
@@ -0,0 +1,25 @@
+# Unicorn server lets you run any Rails application concurrently
+#  by running multiple Ruby processes in a single dyno
+
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
